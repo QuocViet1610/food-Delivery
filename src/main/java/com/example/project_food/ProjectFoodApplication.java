2c6a4ae1fba1d014224ec36221ded457a9148a0c
Quoc Viet <vietddls06@gmail.com>
2024-05-24 21:53:42 +0700

    first commit

---
+++ src/main/java/com/example/project_food/ProjectFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFoodApplication.class, args);
+	}
+
+}
